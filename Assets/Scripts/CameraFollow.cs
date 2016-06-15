9c07bc668b0c970abcb70d9b84be67e5ca9d1459
Simo Larvi <simo.larvi@cs.tamk.fi>
2016-06-15 12:55:26 +0300

    Lisätty ajorata ja putsattu playerInputtia/cameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,11 +4,7 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 	
 	public Transform player;
-	Renderer playerRenderer;
-
 	public Transform cube;
-	Renderer cubeRenderer;
-
 	public float distanceMultiplier;
 
 	private Vector3 middlePoint;
@@ -18,78 +14,36 @@ public class CameraFollow : MonoBehaviour {
 	private float aspectRatio;
 	private float fov;
 	private float tanFov;
-
 	private float margin;
-	public float zoomSpeed;
 
 	// Use this for initialization
 	void Start () {
-		//playerRenderer = player.GetComponent<MeshRenderer> ();
-		//cubeRenderer = cube.GetComponent<MeshRenderer> ();
-
 		aspectRatio = Screen.width / Screen.height;
 		tanFov = Mathf.Tan (Mathf.Deg2Rad * Camera.main.fieldOfView * 0.5f);
 	}
-
-
-	//  ** master branch Update version **
-	/*
-	// Update is called once per frame
-	void Update () {
-
-//		if (!playerRenderer.isVisible || !cubeRenderer.isVisible) {
-//
-//			print ("EI NÄY PRKL");
-//
-//			distancemultip = distancemultip + 0.1f;
-//			transform.position = new Vector3 ((player.transform.position.x + cube.transform.position.x) / 2, Vector3.Distance(player.transform.position,cube.transform.position)*distancemultip, (player.transform.position.z + cube.transform.position.z) / 2);
-//
-//		}
-
-//		float distX = Mathf.Abs(player.transform.position.x - cube.transform.position.x);
-//		float distZ = Mathf.Abs(player.transform.position.z - cube.transform.position.z);
-//
-//		if (distX > distZ) {
-//
-//		}
-//
-//
-//		Vector3 zHelperPlayer = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z);
-//		
-//		Vector3 zHelperCube = new Vector3 (cube.transform.position.x, cube.transform.position.y, player.transform.position.z);
-//
-		
-		transform.position = new Vector3 ((player.transform.position.x + cube.transform.position.x) / 2, Vector3.Distance(player.transform.position, cube.transform.position)*distancemultip, (player.transform.position.z + cube.transform.position.z) / 2);
-
-			//transform.position = new Vector3 ((player.transform.position.x + cube.transform.position.x) / 2, Vector3.Distance(player.transform.position,cube.transform.position)*distancemultip, (player.transform.position.z + cube.transform.position.z) / 2);
-	}
-	*/
 
 	void Update() {
 
 		Vector3 vectorBetweenPlayers = cube.position - player.position;
 		middlePoint = player.position + 0.5f * vectorBetweenPlayers;
 
-		//float xDistance = Mathf.Abs (vectorBetweenPlayers.x);
-		float zDistance = Mathf.Abs (vectorBetweenPlayers.z);
-
-		distanceMultiplier = 1 + zDistance / 90;
-		margin = 2f;
-
 		distanceBetweenPlayers = vectorBetweenPlayers.magnitude;
 		cameraDistance = (distanceBetweenPlayers / 2.0f / aspectRatio) / tanFov;
+
+		float zDistance = Mathf.Abs (vectorBetweenPlayers.z);
+		distanceMultiplier = 1 + zDistance / 90;
+		margin = 2f;
 
 		middlePoint = new Vector3 (middlePoint.x, (cameraDistance + margin) * distanceMultiplier, middlePoint.z);
 
 		//min distance zoom
-		if (middlePoint.y < 20) {
-			middlePoint = new Vector3 (middlePoint.x, 20, middlePoint.z);
+		if (middlePoint.y < 30) {
+			middlePoint = new Vector3 (middlePoint.x, 30, middlePoint.z);
 		}
 	}
 
 	void LateUpdate() {
 		transform.position = middlePoint;
-		//transform.position = Vector3.Lerp(transform.position, middlePoint, Time.deltaTime * zoomSpeed);
 	}
 	
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -15,6 +15,8 @@ public class PlayerInput : MonoBehaviour {
 
 	private Vector3 minimumTurnVelocity;
 
+	private const bool DEBUG = true;
+
 	// Use this for initialization
 	void Start () {
 		playerAccelerateKey = KeyCode.W;
@@ -22,73 +24,45 @@ public class PlayerInput : MonoBehaviour {
 		rigidbody = GetComponent<Rigidbody> ();
 		minimumTurnVelocity = new Vector3 (1, 1, 1);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		GetPlayerInput ();
+		playerAcceleration ();
+		playerTurning ();
+		DebugMoving (DEBUG);
 	}
 
-	/*
-	void GetPlayerInput () {
-		if (Input.GetKey (playerAccelerateKey)) {
-			rigidbody.AddRelativeForce(new Vector3(torque, 0, 0));
-		} else if (Input.GetKey (playerDeccerelateKey)) {
-			rigidbody.AddRelativeForce(new Vector3(-torque * torqueDeccerelationMultiplier, 0, 0));
-		}
-			
-		if (Mathf.Abs (transform.InverseTransformDirection (rigidbody.velocity).x) > minimumSpeedForTurning || Mathf.Abs (transform.InverseTransformDirection (rigidbody.velocity).z) > minimumSpeedForTurning) {
-			myRotation = Input.GetAxis ("Horizontal") * Time.deltaTime * turnSpeed;
-		} else {
-			myRotation = Input.GetAxis ("Horizontal") * Time.deltaTime * turnSpeed * (Mathf.Abs (transform.InverseTransformDirection (rigidbody.velocity).x));
-		}
-
-		transform.Rotate (0, myRotation, 0);
-	}
-	*/
-
-	void GetPlayerInput() {
+	void playerAcceleration() {
 		rigidbody.drag = 0.6f;
 
 		if (Input.GetKey (playerAccelerateKey)) {
-//			if (Vector3.Angle (rigidbody.velocity, gameObject.transform.right) > 30) {
-//				rigidbody.AddRelativeForce(new Vector3(torque, 0, 0) + transform.right);
-//			}
 
 			if (!Input.GetKey (KeyCode.A) && !Input.GetKey (KeyCode.D)) {
-				//Vector3 velo = new Vector3 (rigidbody.velocity.x, rigidbody.velocity.y, rigidbody.velocity.z * 0.9f);
-				//Vector3 velo = transform.TransformDirection(rigidbody.velocity);
 				Vector3 velo = transform.InverseTransformDirection(rigidbody.velocity);
 				velo.z = velo.z * 0.98f;
 				rigidbody.velocity = transform.TransformDirection(velo);
-//				rigidbody.velocity = (new Vector3 (transform.InverseTransformDirection(rigidbody.velocity.x)
-//					, transform.InverseTransformDirection.(rigidbody.velocity.y),
-//					transform.InverseTransformDirection(rigidbody.velocity.z * 0.9f)));
 			}
-
 			rigidbody.AddRelativeForce(new Vector3(torque, 0, 0));
 
-
 		} else if (Input.GetKey (playerDeccerelateKey)) {
-			//rigidbody.AddRelativeForce(new Vector3(-torque * torqueDeccerelationMultiplier, 0, 0));
-			//rigidbody.AddRelativeForce(-(rigidbody.velocity * 0.4f));
 			rigidbody.drag = 1.0f;
 		}
-			
+	}
+
+	void playerTurning() {
 		myRotation = Input.GetAxis ("Horizontal") * Time.deltaTime * turnSpeed;
 
 		if (rigidbody.velocity.magnitude > minimumTurnVelocity.magnitude) {
 			transform.Rotate (0, myRotation, 0);
 		} 
-			
-		print (" angle = " + Vector3.Angle(rigidbody.velocity, gameObject.transform.right));
 
-		if (Vector3.Angle (rigidbody.velocity, gameObject.transform.right) > 30) {
-
-		}
-
-		Debug.DrawRay (transform.position, rigidbody.velocity, Color.red);
-		Debug.DrawRay (transform.position, gameObject.transform.right * 5f, Color.green);
-
-		print ("velocity = " + rigidbody.velocity);
 	}
+
+	void DebugMoving(bool b) {
+		
+		if (b) {
+			Debug.DrawRay (transform.position, rigidbody.velocity, Color.red);
+			Debug.DrawRay (transform.position, gameObject.transform.right * 5f, Color.green);
+		}
+	}
+
 }
