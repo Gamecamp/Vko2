1d0381dd97738bab0e77111f6ea24d537e40515f
iikkap <iikka.pohjola@cs.tamk.fi>
2016-06-16 12:59:41 +0300

    masterman
    yessir

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,16 +3,11 @@ using System.Collections;
 
 public class PlayerInput : MonoBehaviour {
 
-<<<<<<< HEAD
-	KeyCode playerAccelerateKey;
-	KeyCode playerDeccerelateKey;
-	Rigidbody rigidbody;
 	Scorekeeper scorekeeper;
-=======
+
 	private KeyCode playerAccelerateKey;
 	private KeyCode playerDeccerelateKey;
 	private Rigidbody rigidbody;
->>>>>>> refs/remotes/origin/SimonOmaOksa
 
 	private float myRotation;
 	public float turnSpeed;
@@ -26,10 +21,13 @@ public class PlayerInput : MonoBehaviour {
 
 	private const bool DEBUG = true;
 
+	CheckPointChecker checkPointChecker;
+
 
 	// Use this for initialization
 	void Start () {
 		scorekeeper = GameObject.Find ("Scorekeeper").GetComponent<Scorekeeper>();
+		checkPointChecker = GetComponent<CheckPointChecker> ();
 
 		playerAccelerateKey = KeyCode.W;
 		playerDeccerelateKey = KeyCode.S;
@@ -39,25 +37,18 @@ public class PlayerInput : MonoBehaviour {
 	}
 
 	void Update () {
-<<<<<<< HEAD
 		if (scorekeeper.gameGoing) {
-			if (GetComponent<GroundCheck> ().GetIsGrounded ()) {
+			isGrounded = GetComponent<GroundCheck> ().GetIsGrounded ();
+			print (isGrounded);
+
+			if (isGrounded) {
 				playerAcceleration ();
 			}
 			playerTurning ();
+			resetPosition ();
 			DebugMoving (DEBUG);
 		}
-=======
-		isGrounded = GetComponent<GroundCheck> ().GetIsGrounded ();
-		print (isGrounded);
 
-		if (isGrounded) {
-			playerAcceleration ();
-		}
-		playerTurning ();
-		resetPosition ();
-		DebugMoving (DEBUG);
->>>>>>> refs/remotes/origin/SimonOmaOksa
 	}
 
 	void playerAcceleration() {
@@ -78,15 +69,20 @@ public class PlayerInput : MonoBehaviour {
 	}
 
 	void resetPosition() {
-		print (rigidbody.centerOfMass);
-		print (resetTime);
 		if (!isGrounded) {
 			resetTime += Time.deltaTime;
 
 			if (resetTime > 3.0f) {
 				print ("reset");
-				transform.position = new Vector3 (transform.position.x, transform.position.y + 2, transform.position.z);
-				transform.Rotate (new Vector3 (0, 0, transform.rotation.y));
+
+				if (checkPointChecker.GetCurrentPoint() > 0) {
+					transform.position = checkPointChecker.checkPoints [checkPointChecker.GetCurrentPoint () - 1].transform.position;
+					transform.rotation = checkPointChecker.checkPoints [checkPointChecker.GetCurrentPoint () - 1].transform.rotation;
+				} else {
+					transform.position = GameObject.Find ("Finishline").transform.position;
+				}
+				print ("current point :" + checkPointChecker.GetCurrentPoint ());
+
 				resetTime = 0;
 			}
 		} else {
