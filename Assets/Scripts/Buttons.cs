428f9a8f6ada8fd19ef618da9ebed0c916daa657
banbanxaixai <anton.peltoniemi@gmail.com>
2016-06-17 10:37:04 +0300

    Add start screen with two buttons
    Add start screen with two buttons. At the moment both buttons start a 2
    player game. Buttons script has methods for button clicks.

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnePlayerGame() {
+		SceneManager.LoadScene("rofl");
+	}
+
+	public void TwoPlayerGame() {
+		SceneManager.LoadScene("rofl");
+	}
+
+	public void StartScreen() {
+		SceneManager.LoadScene("start");
+	}
+}
